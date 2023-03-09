8790bee9c7a82dba6d7ca08cddda79f3a870e321
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-09 20:19:41 +0200

    backend: checking for users

--- backend/signup.php
+++ backend/signup.php
@@ -8,4 +8,13 @@ $password = $_POST["password"];
 $dob = $_POST["dob"];
 
 $hashed = hash("sha256", $password);
+
+$query = mysql -> prepare ("SELECT * FROM `user` WHERE email = ?, passsword = ?");
+$query -> bind_param("ss", $email, $hashed);
+$query -> execute();
+$result = $query -> get_result();
+
+while($object = $result -> fetch_assoc()){
+    $data = $object;
+}
 ?>
