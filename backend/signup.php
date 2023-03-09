bfdf750841bf03f15a1c0753de11005f2b8e7c84
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-09 20:14:40 +0200

    backend: defining variables

--- backend/signup.php
+++ backend/signup.php
@@ -2,4 +2,10 @@
 
 include('connection.php');
 
+$name = $_POST["name"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$dob = $_POST["dob"];
+
+$hashed = hash("sha256", $password);
 ?>
