201ee8ec94bd06b43ca423693ea1ad0f48f38361
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-09 20:09:42 +0200

    backend: connecting with database

--- backend/connection.php
+++ backend/connection.php
@@ -4,4 +4,11 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 
+$host = 'localhost';
+$db_username = 'root';
+$db_password = null;
+$db_name = 'hospital_db';
+
+$mysql = new mysqli ($host, $db_username, $db_password, $db_name);
+
 ?>
