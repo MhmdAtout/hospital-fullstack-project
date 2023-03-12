da27b6fdede67a329a552217274c93aca9dc07a9
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-12 14:52:25 +0200

    backend: get employyes api

---
+++ backend/get_employees.php
@@ -0,0 +1,19 @@
+<?php
+
+include("connection.php");
+
+$query = $mysql -> prepare("SELECT * FROM `users` WHERE usertype_id = 2");
+$query -> execute();
+$result = $query -> get_result();
+
+while ($object = $result -> fetch_assoc()){
+    $data[] = $object;
+}
+
+$response = [
+    "users" => $data
+];
+
+echo json_encode($response);
+
+?>
