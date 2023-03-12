b3d019de8f516f7c2ab4a8409294e2400975222a
MhmdAtout <mohamad.atout0@icloud.com>
2023-03-12 15:49:00 +0200

    frontend: fetching patients data from database

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -1,4 +1,21 @@
-const users_to_assign = document.getElementById("users_to_assign");
+const patient_to_assign = document.getElementById("patient_to_assign");
+const employee_to_assign = document.getElementById("employee_to_assign");
 const hospital_toassign_to = document.getElementById("hospital_toassign_to");
 
 const baseURL = "http://localhost/hospital-fullstack-project/backend";
+
+axios({
+  method: "get",
+  url: `${baseURL}/get_patients.php`,
+}).then((res) => {
+  patients = res.data.users;
+  console.log(patients);
+
+  patients.forEach((patient) => {
+    const option = document.createElement("option");
+    option.value = patient.id;
+    option.text = `${patient.name}`;
+
+    patient_to_assign.append(option);
+  });
+});
